cdd2e09fc19bc77c498013a7d58bfe3b41df7c6e
al <asaselok4@gmail.com>
2025-01-22 23:21:57 +0300

    the final

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +15,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val buttonSearch = findViewById<Button>(R.id.button_search)
+        val buttonLibrary = findViewById<Button>(R.id.button_library)
+        val buttonSettings = findViewById<Button>(R.id.button_settings)
+
+        buttonSearch.setOnClickListener {
+            startActivity(Intent(this, ActivitySearch::class.java))
+        }
+        buttonLibrary.setOnClickListener {
+            startActivity(Intent(this, ActivityLibrary::class.java))
+        }
+        buttonSettings.setOnClickListener{
+            startActivity(Intent(this, ActivitySettings::class.java))
         }
     }
 }
+
