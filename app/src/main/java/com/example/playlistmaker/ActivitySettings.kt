c0c3e05ecd245e0443266fa324d4fc50135d586a
al <asaselok4@gmail.com>
2025-01-23 03:16:06 +0300

    the final 2

--- app/src/main/java/com/example/playlistmaker/ActivitySettings.kt
+++ app/src/main/java/com/example/playlistmaker/ActivitySettings.kt
@@ -2,7 +2,9 @@ package com.example.playlistmaker
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,9 +13,8 @@ import androidx.core.view.WindowInsetsCompat
 class ActivitySettings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
-        val buttonBack = findViewById<ImageView>(R.id.button_back)
+        val buttonBack = findViewById<ImageButton>(R.id.button_back)
         buttonBack.setOnClickListener { startActivity(Intent(this,MainActivity::class.java)) }
     }
 }
--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -13,7 +13,6 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         val buttonSearch = findViewById<Button>(R.id.button_search)
         val buttonLibrary = findViewById<Button>(R.id.button_library)
