bf5b232cc1fe27221ddaaa910d5f9edf18c59bd1
al <asaselok4@gmail.com>
2025-01-23 03:18:04 +0300

    the final 2

--- app/src/main/java/com/example/playlistmaker/ActivitySettings.kt
+++ app/src/main/java/com/example/playlistmaker/ActivitySettings.kt
@@ -3,12 +3,7 @@ package com.example.playlistmaker
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class ActivitySettings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
